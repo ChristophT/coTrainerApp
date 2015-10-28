21c2c7ad7c45a35f77fa7cbaf323f47847027c2b
ChristophT <thelen.christoph@gmail.com>
2015-10-28 23:55:56 +0100

    SportlerListe - Sportler und FAB anzeigen.

--- www/js/modules/sportlerListe/SportlerListeController.js
+++ www/js/modules/sportlerListe/SportlerListeController.js
@@ -3,5 +3,13 @@
 angular.module('sportlerListe').controller('SportlerListeController', function (SportlerService) {
     var vm = this;
 
-    vm.test = 'geht';
+    vm.sportler = SportlerService.aktiveSportler;
+
+    vm.addSportler = function (name) {
+        SportlerService.addSportler(name);
+    };
+
+    vm.deleteSportler = function (sportler) {
+        SportlerService.deleteSportler(sportler);
+    };
 });
