4d44e999041263b7497e8a08285311d25fb1d6c8
ChristophT <thelen.christoph@gmail.com>
2015-10-28 23:57:58 +0100

    Unterscheidung Sportler/aktive Sportler. Sportler hinzufügen/löschen.

--- www/js/index.js
+++ www/js/index.js
@@ -19,3 +19,14 @@ var app = {
 };
 
 app.initialize();
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
